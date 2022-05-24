044a3422bbdecece0a212d51a81965aa6842c32d
Dan Bond <danbond@protonmail.com>
2022-05-24 12:53:47 -0700

    create service definition
    Signed-off-by: Dan Bond <danbond@protonmail.com>

--- cmd/http.go
+++ cmd/http.go
@@ -7,18 +7,18 @@ import (
 	"os"
 )
 
-// newHTTPServer configures a HTTP server with all required handle funcs
-// and their dependencies
-func newHTTPServer(port int, fail bool, stop chan os.Signal) *http.Server {
+// NewHTTPServer configures a HTTP server with all required handle funcs
+// and their dependencies.
+func NewHTTPServer(port int, stop chan os.Signal) *http.Server {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", handler(fail))
+	mux.HandleFunc("/healthz", handler)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%v", port),
 		Handler: mux,
 	}
 
-	log.Printf("starting http server [addr: localhost:%d]", port)
+	log.Printf("starting http server, addr: localhost:%d", port)
 
 	go func() {
 		if err := server.ListenAndServe(); err != nil {
@@ -31,50 +31,21 @@ func newHTTPServer(port int, fail bool, stop chan os.Signal) *http.Server {
 	return server
 }
 
-// statusResponseWriter wraps the standard http.ResponseWriter and records
-// the status when calling WriteHeader
-type statusResponseWriter struct {
-	status int
-	http.ResponseWriter
-}
-
-func (w *statusResponseWriter) WriteHeader(status int) {
-	w.status = status
-	w.ResponseWriter.WriteHeader(status)
-}
-
 // httpLog logs specific HTTP request information
 func httpLog(r *http.Request, status int) {
 	log.Printf(`"%s %s %s" %d %s`, r.Method, r.URL.Path, r.Proto, status, http.StatusText(status))
 }
 
-func handler(fail bool) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		srw := &statusResponseWriter{
-			ResponseWriter: rw,
-		}
-
-		switch r.Method {
-		case http.MethodGet:
-			healthz(srw, fail)
-		default:
-			status := http.StatusText(http.StatusMethodNotAllowed)
-			http.Error(srw, status, http.StatusMethodNotAllowed)
-		}
-
-		httpLog(r, srw.status)
-	}
-}
-
-func healthz(rw http.ResponseWriter, fail bool) {
-	rw.Header().Set("Content-Type", "text/plain")
-
-	if fail {
-		rw.WriteHeader(http.StatusServiceUnavailable)
-		fmt.Fprintln(rw, "FAILING")
+func handler(rw http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		status := http.StatusMethodNotAllowed
+		http.Error(rw, http.StatusText(status), http.StatusMethodNotAllowed)
+		httpLog(r, status)
 		return
 	}
 
+	rw.Header().Set("Content-Type", "text/plain")
 	rw.WriteHeader(http.StatusOK)
 	fmt.Fprintln(rw, "OK")
+	httpLog(r, http.StatusOK)
 }
---
+++ cmd/service.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	consul "github.com/hashicorp/consul/api"
+)
+
+type Service struct {
+	// Unique Service ID.
+	id string
+
+	// Unique Servie address.
+	addr string
+
+	// Consul client.
+	client *consul.Client
+
+	// Local HTTP server.
+	server *http.Server
+
+	// Channel for sending stop commands.
+	stop chan os.Signal
+}
+
+func NewService(id string, port int, stop chan os.Signal) (Service, error) {
+	s := Service{
+		id:   id,
+		addr: fmt.Sprintf("http://%v:%v", id, port),
+		stop: stop,
+	}
+
+	// configure consul client
+	config := &consul.Config{
+		Address: "consul-agent:8500",
+	}
+	var err error
+	s.client, err = consul.NewClient(config)
+	if err != nil {
+		return s, fmt.Errorf("error creating consul client: %v", err)
+	}
+
+	// Start local HTTP server.
+	s.server = NewHTTPServer(port, stop)
+
+	return s, nil
+}
+
+func (s Service) Start() {
+	// attempt to register new service with local consul agent
+	if err := s.registerConsulService(); err != nil {
+		log.Fatalf("error registering service with consul: %v", err)
+	}
+
+	// attempt to register as leader and start background tasks
+	if err := s.registerConsulLeader(); err != nil {
+		log.Fatalf("error registering service as leader: %v", err)
+	}
+
+	s.getRegisteredConsulServices()
+}
+
+func (s Service) Shutdown() error {
+	// attempt to deregister service on shutdown
+	if err := s.client.Agent().ServiceDeregister(s.id); err != nil {
+		return fmt.Errorf("error deregistering service from consul: %v", err)
+	}
+
+	// attempt to gracefully shutdown HTTP server
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.server.Shutdown(ctx); err != nil {
+		return fmt.Errorf("error shutting down http server: %v", err)
+	}
+
+	return nil
+}
+
+// registerConsulService attempts to register a service and health check
+// with a local consul agent
+func (s Service) registerConsulService() error {
+	// create consul api service
+	svc := &consul.AgentServiceRegistration{
+		Address: s.addr,
+		ID:      s.id,
+		Name:    svcName,
+		Tags: []string{
+			"demo",
+			"api",
+		},
+		Check: &consul.AgentServiceCheck{
+			Interval: "5s",
+			HTTP:     fmt.Sprintf("%v/healthz", s.addr),
+			Method:   http.MethodGet,
+			Name:     "/healthz",
+			Timeout:  "1s",
+		},
+	}
+
+	// attempt to register new service with local consul agent
+	return s.client.Agent().ServiceRegister(svc)
+}
+
+// registerConsulLeader attempts to aquire a lock session with a unique id
+func (s Service) registerConsulLeader() error {
+	sessionID, _, err := s.client.Session().Create(&consul.SessionEntry{
+		Name:     fmt.Sprintf("service/%s/leader", svcName),
+		Behavior: "delete",
+		TTL:      "10s",
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	done := make(chan struct{})
+	go func() {
+		if err := s.client.Session().RenewPeriodic("10s", sessionID, nil, done); err != nil {
+			log.Printf("error renewing lock session: %v", err)
+			s.stop <- os.Kill
+			return
+		}
+	}()
+
+	go func() {
+		for {
+			leader, _, err := s.client.KV().Acquire(&consul.KVPair{
+				Key:     fmt.Sprintf("service/%s/leader", svcName),
+				Value:   []byte(s.id),
+				Session: sessionID,
+			}, nil)
+			if err != nil {
+				log.Printf("error acquiring lock: %v", err)
+				close(done)
+				s.stop <- os.Kill
+				return
+			}
+
+			if leader {
+				log.Println("lock acquired, registered as leader")
+			}
+
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	return nil
+}
+
+func (s Service) getRegisteredConsulServices() {
+	go func() {
+		catalog := s.client.Catalog()
+
+		for {
+			opts := &consul.QueryOptions{}
+			svcs, _, err := catalog.Services(opts)
+			if err != nil {
+				log.Println("failed to get service catalog, will retry")
+			}
+
+			for svc := range svcs {
+				if svc == svcName {
+					catSvcs, _, err := catalog.Service(svc, "", opts)
+					if err != nil {
+						log.Printf("failed to get service details: %s", err)
+						continue
+					}
+
+					for _, catSvc := range catSvcs {
+						if catSvc.ServiceID != s.id {
+							log.Printf("discovered new service: %s", catSvc.ServiceID)
+						}
+					}
+				}
+			}
+
+			time.Sleep(30 * time.Second)
+		}
+	}()
+}
